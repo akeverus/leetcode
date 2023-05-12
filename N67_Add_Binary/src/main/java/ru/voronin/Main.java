b0783b647d6f399c36689dfaaebe8be615c1fe98
Sergey Voronin <sergey.voronin@rtlabs.ru>
2023-05-12 17:52:52 +0300

    67 - Add Binary

---
+++ N67_Add_Binary/src/main/java/ru/voronin/Main.java
@@ -0,0 +1,8 @@
+package ru.voronin;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.addBinary("1010101010010", "100000011010"));
+    }
+}
