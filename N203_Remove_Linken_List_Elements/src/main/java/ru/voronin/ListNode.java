33532bb90bfa1f6a0e63a31d94026c0cc50adc5e
Sergey Voronin <sergey.voronin@rtlabs.ru>
2023-06-06 21:37:05 +0300

    203 - Remove Linked List Elements

---
+++ N203_Remove_Linken_List_Elements/src/main/java/ru/voronin/ListNode.java
@@ -0,0 +1,29 @@
+package ru.voronin;
+
+import java.util.Objects;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ListNode listNode = (ListNode) o;
+        return val == listNode.val;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(val);
+    }
+}
